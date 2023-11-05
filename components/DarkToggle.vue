4b49ae5d929902da6d086b70c65a64c00c60c3d2
neequu <nist1701@gmail.com>
2023-11-05 21:41:28 +0300

    feat: add tdesign icons

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -16,6 +16,6 @@ function toggleDark() {
 
 <template>
   <button class="!outline-none" @click="toggleDark">
-    <div class="i-carbon-sun dark:i-carbon-moon text-2xl" />
+    <div class="dark:i-tdesign-moon i-tdesign-sunny text-2xl" />
   </button>
 </template>
