ff53107770eeeab8a3b4bffe701041ac25a49f0a
neequu <nist1701@gmail.com>
2023-11-12 03:04:11 +0300

    feat: add blurred examples preview

--- components/Examples.vue
+++ components/Examples.vue
@@ -3,9 +3,10 @@
 </script>
 
 <template>
-  <div>
+  <div class="relative">
     <NuxtImg width="600" format="webp" src="/examples-preview.jpg" />
     <NuxtImg width="600" format="webp" src="/examples-preview2.jpg" />
+    <div class="absolute inset-0 bg-dark bg-opacity-50 backdrop-blur-2" />
   </div>
 </template>
 
