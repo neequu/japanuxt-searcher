511423dc6cd34596459a225142b1b2582346b82e
neequu <nist1701@gmail.com>
2023-12-03 19:39:10 +0300

    chore: add cookies to supabase req

--- composables/supabase.ts
+++ composables/supabase.ts
@@ -3,6 +3,7 @@ import type { Database } from '~/supabase'
 export async function findWord(word: string): Promise<{ data: Ref<Database['public']['Tables']['user_words']['Row']> }> {
   return useFetch(`/api/supabase/user-words/${word}`, {
     key: word,
+    headers: useRequestHeaders(['cookie']),
     getCachedData(key) {
       return useNuxtApp().payload.data[key] || useNuxtApp().static.data[key]
     },
@@ -12,6 +13,7 @@ export async function findWord(word: string): Promise<{ data: Ref<Database['publ
 export async function getUserWords(): Promise<{ data: Ref<Database['public']['Tables']['user_words']['Row'][]> }> {
   return useFetch(`/api/supabase/user-words`, {
     key: 'userWords',
+    headers: useRequestHeaders(['cookie']),
     getCachedData(key) {
       return useNuxtApp().payload.data[key] || useNuxtApp().static.data[key]
     },
@@ -19,12 +21,13 @@ export async function getUserWords(): Promise<{ data: Ref<Database['public']['Ta
 }
 
 export async function saveWord(word: string) {
-  return $fetch(`/api/supabase/user-words/${word}`, { method: 'post' })
+  return $fetch(`/api/supabase/user-words/${word}`, { headers: useRequestHeaders(['cookie']), method: 'post' })
 }
 export async function deleteWord(word: string, id: number) {
   return $fetch(`/api/supabase/user-words/${word}`, {
   // @ts-expect-error nuxt ts problem
     method: 'delete',
     body: { id },
+    headers: useRequestHeaders(['cookie']),
   })
 }
